c1366d2a2dd9e1c5bce527b04b68cfe65a379aa2
kanreuthai <jansaichi@gmail.com>
2018-07-03 11:16:10 +0700

    Data integer

---
+++ src/lab3/ex1.java
@@ -0,0 +1,22 @@
+package lab3;
+
+public class ex1 {
+    public static void main(String[] args) {
+
+        int x;
+        x = 10;
+        System .out.println(x);
+        x = 20;
+        System .out.println(x);
+        int y = 30;
+        System .out.println(y);
+        int t;
+             t = x+ y;
+        System .out.println(x+y);
+        System.out.println(y-x);
+
+    }//main
+
+
+
+}
