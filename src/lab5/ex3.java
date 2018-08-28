4f8c74e1e6bc96782b03cf8376e57b7f4e087b71
kanreuthai <jansaichi@gmail.com>
2018-08-28 11:07:04 +0700

    lab5 num

---
+++ src/lab5/ex3.java
@@ -0,0 +1,15 @@
+package lab5;
+
+public class ex3 {
+    public static  void main (String[]args){
+        System.out.println(divide(10.0,5));
+
+    }//main
+    public static int divide(int x,int y){
+        return  x /y;
+    }
+    public static double divide(double x,double y){
+        return  x /y;
+    }
+
+}
