9f18c15c654abb693cc59d9eebd55b43ef451061
kanreuthai <jansaichi@gmail.com>
2018-10-02 11:19:43 +0700

    lab7 ; Rectangle

---
+++ src/lab7/StudentApp.java
@@ -0,0 +1,15 @@
+package lab7;
+
+public class StudentApp {
+    public static void main(String[] args) {
+    Student std = new Student("jan",
+            "034",
+    "jansaichi@gmail.com",
+            "0986740283",
+            21);
+
+   System.out.println(std.toString());
+
+    }
+
+}//class
